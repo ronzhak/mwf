03ee023494b8f64945d50ce3ef6106223e17446b
Dmitriy Ronzhin <dronzhin@ics.perm.ru>
2019-11-22 19:18:22 +0500

    Init

---
+++ frontend/mwf-app/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+<article v-for="(fed, idx) in federations" :key="idx">
+<h1>{{ fed.name }}</h1>
+</article>
+</div>
+</template>
+
+<script>
+import { db } from '../main'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      federations: []
+    }
+  },
+  firestore () {
+    return {
+      federations: db.collection('federations').orderBy('name')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ frontend/mwf-app/src/main.js
@@ -0,0 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import {VueFire} from 'vuefire'
+import App from './App'
+import router from './router'
+
+import {firebase} from 'firebase/app'
+import 'firebase/firestore'
+
+Vue.use(VueFire)
+
+firebase.initializeApp({
+  projectId: 'mwf-app-fb',
+  databaseURL: 'https://mwf-app-fb.firebaseio.com'
+})
+export const db = firebase.firestore()
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
